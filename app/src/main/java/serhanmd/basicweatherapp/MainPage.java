fb1ce5650bc58103a81dbcc128fad8245a49074b
Mohamed Serhan <mohamedserhan7@gmail.com>
2018-06-25 12:26:39 +0300

    GSON integration fully supported. V 0.3 -Added Comments to organize code -Back to full functionality before the GSON integration (Back to square one) -Has enough data for up to 5 days of weather goodness

--- app/src/main/java/serhanmd/basicweatherapp/MainPage.java
+++ app/src/main/java/serhanmd/basicweatherapp/MainPage.java
@@ -1,56 +1,42 @@
 package serhanmd.basicweatherapp;
 
 //Imports
-import android.annotation.SuppressLint;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.lang.reflect.Type;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collection;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import serhanmd.basicweatherapp.WeatherData.Data;
 
+
 public class MainPage extends AppCompatActivity {
 
     //Variables
     public static final String TAG = "MoLog:";
     TextView changeTemp;
-    Data[] weather;
+    Gson gson = new Gson();
+    Data data;
 
     //Executes whatever when the activity is created
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
         changeTemp = findViewById(R.id.tempValue);
-        //Executes getJson method, which can get data from any city depending on the name
         Log.i(TAG,"Process started!");
-        new JSONFeedArrayTask().execute();
+        new JSONFeedArrayTask().execute(); //This command allows the class to run and make URL requests without crashing
 
     }
 
-    //Reads the URL and extracts the JSON file to a local variable
+    //Reads the URL and extracts the JSON file to a local variable, ONLY HARD CODED VALUE SO FAR
     public class JSONFeedArrayTask extends AsyncTask<String, Void, String> {
 
         @Override
@@ -59,22 +45,26 @@ public class MainPage extends AppCompatActivity {
             try {
                 Log.i(TAG,"Initializing Variables...");
                 OkHttpClient client = new OkHttpClient();
+                //Uses OkHttp to request access to the URL to read the data from OpenWeatherAPI website
                 Request request = new Request.Builder()
                         .url("http://api.openweathermap.org/data/2.5/forecast?q=Kuwait%20City&APPID=361e3863fb571305e306d4be3472954b")
                         .build();
                 Log.i(TAG,"Connecting to website");
+                //Accesses the website and saves the data
                 Response response = client.newCall(request).execute();
+                //Saves the JSON data file into a string
                 String result = response.body().string();
                 Log.d(TAG, result);
                 Log.i(TAG,"Generating Gson...");
-                Gson gson = new Gson();
                 Log.i(TAG,"Packing data into arrays step 1");
                 //Type collectionType = new TypeToken<Collection<Data>>() {}.getType();
                 Log.i(TAG,"Packing data into arrays step 2");
-               // Collection<Data> enums = gson.fromJson(result, collectionType);
-                Data data = gson.fromJson(result, Data.class);
+                // Collection<Data> enums = gson.fromJson(result, collectionType);
+
+                //Turns the JSON String into a java object using GSON
+                data = gson.fromJson(result, Data.class);
                 Log.i(TAG,"Packing data into arrays step 3");
-                Log.d(TAG, ""+data.getCnt());
+
                 Log.i(TAG, "Process Complete!");
 
             } catch(IOException e) {
@@ -82,6 +72,14 @@ public class MainPage extends AppCompatActivity {
             }
 
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            int tmp = (int) data.getList().get(0).getMain().getTemp(); //HOW TO ACCESS THE DATA!!!!!!!!!!!!
+            changeTemp.setText(""+tmp + " " + (char) 0x00B0+"C");
+            Toast.makeText(getApplicationContext(),"Weather Found", Toast.LENGTH_SHORT).show();
         }
     }
 
