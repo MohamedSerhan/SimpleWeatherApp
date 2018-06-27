c667e4e229e2f2405fd5e742b237eb86d63a2fb4
Mohamed Serhan <mohamedserhan7@gmail.com>
2018-06-27 12:17:17 +0300

    Added new comments to explain code Added new variables Added Picasso dependency for loading images UI has more data -City name can now be changed UI looks better -Added icon for future feature

--- app/src/main/java/serhanmd/basicweatherapp/MainPage.java
+++ app/src/main/java/serhanmd/basicweatherapp/MainPage.java
@@ -1,16 +1,22 @@
 package serhanmd.basicweatherapp;
 
 //Imports
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -23,15 +29,27 @@ public class MainPage extends AppCompatActivity {
     //Variables
     public static final String TAG = "MoLog:";
     TextView changeTemp;
+    ImageView changeIcon;
+    TextView changeCity;
     Gson gson = new Gson();
     Data data;
 
     //Executes whatever when the activity is created
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Starts the selected layout as the first page when app opens
         setContentView(R.layout.activity_main_page);
+
+        //Set these variables to the proper IDs of the views found on the app interface
         changeTemp = findViewById(R.id.tempValue);
+        changeIcon = findViewById(R.id.icon);
+        changeCity = findViewById(R.id.cityName);
+        //changeTemp.setVisibility(View.INVISIBLE);
+        //Picasso.get().load("http://openweathermap.org/img/w/10d.png").into(changeIcon);
+
         Log.i(TAG,"Process started!");
+
         new JSONFeedArrayTask().execute(); //This command allows the class to run and make URL requests without crashing
 
     }
