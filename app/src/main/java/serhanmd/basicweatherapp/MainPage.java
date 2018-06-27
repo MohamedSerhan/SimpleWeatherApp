daf01be9f6551cc39f8d08d98774c39f8ef3a69f
Mohamed Serhan <mohamedserhan7@gmail.com>
2018-06-27 14:03:11 +0300

    New method to set visibility of views New method to change icon from JSON New button added to refresh data Refresh feature added! UI Improvements -Icon working properly -Conditions now included -Temperature text is much bigger -Extra information added at the bottom    []Description of condition    []Date of the day
    New method to change date format
    New method to output data from JSON to the interface
    More comments added

--- app/src/main/java/serhanmd/basicweatherapp/MainPage.java
+++ app/src/main/java/serhanmd/basicweatherapp/MainPage.java
@@ -1,12 +1,12 @@
 package serhanmd.basicweatherapp;
 
 //Imports
-import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,8 +15,9 @@ import com.google.gson.Gson;
 import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -29,8 +30,14 @@ public class MainPage extends AppCompatActivity {
     //Variables
     public static final String TAG = "MoLog:";
     TextView changeTemp;
-    ImageView changeIcon;
     TextView changeCity;
+    TextView changeCondition;
+    TextView changeDescription;
+    TextView changeDate;
+    TextView descriptionTxt;
+    TextView dateTxt;
+    ImageView changeIcon;
+    Button refresh;
     Gson gson = new Gson();
     Data data;
 
@@ -43,15 +50,58 @@ public class MainPage extends AppCompatActivity {
 
         //Set these variables to the proper IDs of the views found on the app interface
         changeTemp = findViewById(R.id.tempValue);
+        changeCondition = findViewById(R.id.condText);
         changeIcon = findViewById(R.id.icon);
         changeCity = findViewById(R.id.cityName);
-        //changeTemp.setVisibility(View.INVISIBLE);
-        //Picasso.get().load("http://openweathermap.org/img/w/10d.png").into(changeIcon);
+        changeDescription = findViewById(R.id.descValue);
+        changeDate = findViewById(R.id.dateValue);
+        descriptionTxt = findViewById(R.id.descText);
+        dateTxt = findViewById(R.id.dateText);
+        refresh = findViewById(R.id.refreshButton);
+
+        setViewVisibility(false);
+
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new JSONFeedArrayTask().execute();
+            }
+        });
 
         Log.i(TAG,"Process started!");
 
         new JSONFeedArrayTask().execute(); //This command allows the class to run and make URL requests without crashing
 
+    }
+    //Sets all the views to visible for true and invisible for false
+    public void setViewVisibility(boolean bool) {
+        if(bool) {
+            changeTemp.setVisibility(View.VISIBLE);
+            changeCondition.setVisibility(View.VISIBLE);
+            changeIcon.setVisibility(View.VISIBLE);
+            changeCity.setVisibility(View.VISIBLE);
+            changeDescription.setVisibility(View.VISIBLE);
+            changeDate.setVisibility(View.VISIBLE);
+            descriptionTxt.setVisibility(View.VISIBLE);
+            dateTxt.setVisibility(View.VISIBLE);
+            refresh.setVisibility(View.VISIBLE);
+        }
+        else {
+            changeTemp.setVisibility(View.INVISIBLE);
+            changeCondition.setVisibility(View.INVISIBLE);
+            changeIcon.setVisibility(View.INVISIBLE);
+            changeCity.setVisibility(View.INVISIBLE);
+            changeDescription.setVisibility(View.INVISIBLE);
+            changeDate.setVisibility(View.INVISIBLE);
+            descriptionTxt.setVisibility(View.INVISIBLE);
+            dateTxt.setVisibility(View.INVISIBLE);
+            refresh.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    //Changes 
+    public void changeIconImage(String iconCode) {
+        Picasso.get().load("http://openweathermap.org/img/w/" + iconCode + ".png").into(changeIcon);
     }
 
     //Reads the URL and extracts the JSON file to a local variable, ONLY HARD CODED VALUE SO FAR
@@ -95,10 +145,43 @@ public class MainPage extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            int tmp = (int) data.getList().get(0).getMain().getTemp(); //HOW TO ACCESS THE DATA!!!!!!!!!!!!
-            changeTemp.setText(""+tmp + " " + (char) 0x00B0+"C");
+            setDataToViews(0);
             Toast.makeText(getApplicationContext(),"Weather Found", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    //Sets data to interface of APP
+    public void setDataToViews(int listNum) {
+        int tmp = (int) data.getList().get(listNum).getMain().getTemp(); //HOW TO ACCESS THE DATA!!!!!!!!!!!!
+        changeCity.setText(data.getCity().getName());
+        changeIconImage(data.getList().get(listNum).getWeather().get(0).getIcon());
+        changeCondition.setText(data.getList().get(listNum).getWeather().get(0).getMain());
+        changeTemp.setText(""+tmp + " " + (char) 0x00B0+"C");
+        changeDescription.setText(data.getList().get(listNum).getWeather().get(0).getDescription());
+        changeDate.setText(formatDate(data.getList().get(listNum).getDtTxt().substring(0,10)));
+        setViewVisibility(true);
+
+    }
+
+    //Changes the format of date from (yyyy-mm-dd) to (mmmm dd, yyyy)
+    public String formatDate(String date) {
+        //Extracts only necessary information from date
+        String dateStr = date.substring(0,10);
+
+        //Prep for date format change
+        SimpleDateFormat curFormater = new SimpleDateFormat("yyyy-MM-dd");
+        Date dateObj = null;
+        //Changes the date to data the SimpleDateFormat can understand
+        try {
+            dateObj = curFormater.parse(dateStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        //Changes the date to desired format
+        SimpleDateFormat postFormater = new SimpleDateFormat("MMMM dd, yyyy");
+        String newDateStr = postFormater.format(dateObj);
+
+        return newDateStr;
     }
 
 }
